2e7feaf89b8838357b0011d167e4e74c2f6be257
lolox <loris.martinez78500@gmail.com>
2023-07-01 18:59:17 +0200

    fin du niveau 1 et debut du niveau 2 du jour 2 du challenge 30 jours des JavascriptfileOfTheDayOne

---
+++ Days/dayOne/JavascriptfileOfTheDayOne.js
@@ -0,0 +1,44 @@
+// you will find the javascript code for the exercices of the day one of the 30 days of javascript challenge
+
+let fil = 'string', vraiFaux = true , indefini , nule = null;
+
+function question4() {
+console.log(typeof fil, vraiFaux, indefini, nule);
+}
+
+let noValue1, noValue2, noValue3, noValue4;
+function consoleQuestion5(){
+    console.log(noValue1, noValue2, noValue3, noValue4);
+} 
+
+let value1 = 1, value2 = 2, value3 = 3, value4 = 4 ;
+
+function consoleQuestion6(){
+    console.log(value1, value2, value3, value4);
+} 
+
+let lastName = 'MARTINEZ';
+let firstName = 'Loris';
+let sexe = 'man';
+let nationalitie = 'french';
+let age = 21;
+
+function consoleQuestion7(){
+    console.log(lastName +' '+ firstName +' '+ sexe +' '+ nationalitie +' '+ age);
+} 
+
+function consoleQuestion8(){
+    console.log(lastName);
+    console.log(firstName);
+    console.log(sexe);
+    console.log(nationalitie);
+    console.log(age);
+} 
+
+
+let myAge = 20;
+let yourAge = 19;
+
+function consoleQuestion10() {
+    console.log("i'm " + myAge + " years old and you are " + yourAge +"years old");
+}
